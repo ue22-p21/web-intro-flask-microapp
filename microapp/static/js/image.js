f0366c0fc1bab274649204db0d696b2fb2eaa4b0
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2021-01-26 13:29:01 +0100

    image.js : . clic and drag within the image: shows current (x, y) . mouseup: sends back request for server to compute area, which gets displayed

--- microapp/static/js/image.js
+++ microapp/static/js/image.js
@@ -1,3 +1,90 @@
-function armMouseFollower(image_id, x_id, y_id, aread_id) {
-    console.log(`armMouseFollower with id=${image_id}`);
+"use strict"
+
+let COLOR = "pink"
+
+function armMouseFollower(image_id, x_id, y_id, area_id) {
+    let elt = document.getElementById(image_id);
+    // preserve color
+    let color = document.body.style.backgroundColor;
+    let [x1, y1] = [0, 0];
+
+    let isMouseDown = false;
+    elt.onmousedown = function (event) {
+        // remove default behaviour which is to drag image
+        event.preventDefault();
+        document.body.style.backgroundColor = COLOR;
+        isMouseDown = true;
+        [x1, y1] = showCoordinates(event, image_id, x_id, y_id);
+    }
+    elt.onmouseup = function (event) {
+        event.preventDefault();
+        document.body.style.backgroundColor = color;
+        isMouseDown = false;
+        showArea(event, image_id, area_id, x1, y1);
+    }
+    elt.onmousemove = function (event) {
+        event.preventDefault();
+        if (isMouseDown) {
+            showCoordinates(event, image_id, x_id, y_id);
+        }
+    }
+ }
+// compute mouse coordinates relative to a DOM elt
+function relativePosition(elt) {
+    if (typeof (elt.offsetParent) != "undefined") {
+        let pos_x = 0;
+        let pos_y = 0;
+        for (; elt; elt = elt.offsetParent) {
+            pos_x += elt.offsetLeft;
+            pos_y += elt.offsetTop;
+        }
+        return [pos_x, pos_y];
+    } else {
+        return [elt.x, elt.y];
+    }
+ }
+
+function showCoordinates(event, image_id, x_id, y_id) {
+    let [pos_x, pos_y] = getRelativeMouse(event, image_id);
+    document.getElementById(x_id).innerHTML = pos_x;
+    document.getElementById(y_id).innerHTML = pos_y;
+    return [pos_x, pos_y];
+}
+
+function getRelativeMouse(event, image_id) {
+    let elt = document.getElementById(image_id);
+    let [rel_x, rel_y] = relativePosition(elt);
+
+    let pos_x = 0;
+    let pos_y = 0;
+    if (!event) event = window.event;
+    if (event.pageX || event.pageY) {
+        pos_x = event.pageX;
+        pos_y = event.pageY;
+    } else if (event.clientX || event.clientY) {
+        pos_x = event.clientX + document.body.scrollLeft
+            + document.documentElement.scrollLeft;
+        pos_y = event.clientY + document.body.scrollTop
+            + document.documentElement.scrollTop;
+    }
+    return [pos_x - rel_x, pos_y - rel_y];
+ }
+
+function showArea(event, image_id, area_id, x1, y1) {
+    let [x2, y2] = getRelativeMouse(event, image_id);
+
+    // post a GET request back to the same server
+    const http = new XMLHttpRequest();
+    const url=`/api/image/area/${image_id}/${x1}/${y1}/${x2}/${y2}`;
+    http.open("GET", url);
+    http.send();
+
+    // how to behave when the answer comes back
+    http.onreadystatechange = (event) => {
+        // the answer is a JSON encoded Python dict
+        let response = JSON.parse(http.responseText);
+        // which results in a JavaScript regular object
+        // that has the 'area' property
+        document.getElementById(area_id).innerHTML = response.area;
+    }
 }
